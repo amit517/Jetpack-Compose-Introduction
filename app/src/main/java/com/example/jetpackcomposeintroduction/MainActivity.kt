5ecb2662c9cbf964b13ffacd54f00a78c6efc7d8
Amit <amit@sharetrip.net>
2021-12-19 16:25:49 +0600

    Text view

--- app/src/main/java/com/example/jetpackcomposeintroduction/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeintroduction/MainActivity.kt
@@ -1,36 +1,47 @@
 package com.example.jetpackcomposeintroduction
 
+import android.icu.text.CaseMap
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcomposeintroduction.ui.theme.JetpackComposeIntroductionTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Greeting(name = "Amit Kundu")
-        }
+        setContent { Title() }
     }
 }
 
 @Composable
-fun Greeting(name: String) =
-    Surface(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
-        Text(text = "Hello $name!")
-    }
+fun Title() {
+    val context = LocalContext.current
+    Text(
+        text = "Simplified Coding",
+        fontSize = 14.sp,
+        color = colorResource(id = R.color.purple_700),
+        fontFamily = FontFamily.Cursive,
+        modifier = Modifier.clickable {
+            Toast.makeText(context,"Title Clicked", Toast.LENGTH_LONG).show()
+        }
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    JetpackComposeIntroductionTheme {
-        Greeting("Amit Kundu")
+fun DefaultPreview(){
+    Surface(Modifier.fillMaxSize()) {
+        Title()
     }
 }
