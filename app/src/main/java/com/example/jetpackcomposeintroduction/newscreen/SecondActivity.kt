a4871ca8a5d0587f9f13fb975f983a2b2d3aa0e3
Amit Kundu <amit.kundu@dreambroker.fi>
2023-07-29 11:31:09 +0300

    Customized font with typography

--- app/src/main/java/com/example/jetpackcomposeintroduction/newscreen/SecondActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeintroduction/newscreen/SecondActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -17,15 +19,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposeintroduction.R
 import com.example.jetpackcomposeintroduction.ui.theme.JetpackComposeIntroductionTheme
+import com.example.jetpackcomposeintroduction.ui.theme.Typography
 
 class SecondActivity : ComponentActivity() {
 
@@ -74,7 +78,10 @@ fun GreetingsCard(name: String, topPadding: Int) {
 
 @Composable
 fun profileCard(name : String) {
-    Row(modifier = Modifier.padding(top = 16.dp, start = 16.dp)) {
+    Row(modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp).fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Image(painter = painterResource(id = R.drawable.my_image), contentDescription = null, modifier = Modifier
             // Set image size to 40 dp
             .size(40.dp)
@@ -88,12 +95,12 @@ fun profileCard(name : String) {
 
         Column() {
             Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
-                Text(text = name, color = MaterialTheme.colors.secondaryVariant, modifier = Modifier.padding(4.dp))
+                Text(fontStyle = FontStyle.Italic, text = name, style = Typography.h3, color = MaterialTheme.colors.secondaryVariant, modifier = Modifier.padding(4.dp))
             }
 
             Spacer(modifier = Modifier.height(4.dp))
 
-            Text(text = "amit517", style = MaterialTheme.typography.subtitle2)
+            Text(modifier = Modifier.padding(start = 2.dp),text = "amit517", style = MaterialTheme.typography.subtitle2)
         }
     }
 }
